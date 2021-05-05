24693b709422c2751d08ae27c1bf8251148b42cd
CGuilliman <cguilliman@gmail.com>
2021-05-05 14:09:48 +0300

    0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 
     setup(
         name='django-ib-cache',
-        version='0.0.3',
+        version='0.0.4',
         long_description_content_type='text/x-rst',
         packages=[pkj_name] + [pkj_name + '.' + x for x in find_packages(pkj_name)],
         include_package_data=True,
